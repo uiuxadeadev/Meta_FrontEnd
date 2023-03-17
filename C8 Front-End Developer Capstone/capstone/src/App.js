7efd4838e8b8b511aa3b1329ff0355e131f52127
Tai <taiu.engineer@gmail.com>
2023-03-18 02:38:34 +0900

    footer

--- C8 Front-End Developer Capstone/capstone/src/App.js
+++ C8 Front-End Developer Capstone/capstone/src/App.js
@@ -11,8 +11,11 @@ import {
 } from "@chakra-ui/react";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
 import Reservations from "./components/Reservations";
 import Order from "./components/Order";
+import Footer from "./components/Footer";
 
 import logo from "./assets/Logo.svg";
 
@@ -39,21 +42,6 @@ function App() {
         />
       </head>
 
-      {/* <Router>
-        <div className="App">
-          <Navigation setActivePage={setActivePage} />
-          <Home id="Home" />
-          <About id="About" />
-          <Menu id="Menu" />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/reservations" element={<Reservations />} />
-            <Route path="/order" element={<Order />} />
-          </Routes>
-        </div>
-      </Router> */}
       <ChakraProvider>
         <Router>
           <Box bg="gray.200" py="2">
@@ -76,81 +64,10 @@ function App() {
             <Route path="/order" element={<Order />} />
           </Routes>
         </Router>
+        <Footer />
       </ChakraProvider>
     </>
   );
 }
 
-// function Navigation(props) {
-//   return (
-//     <nav>
-//       <ul>
-//         <li onClick={() => props.setActivePage("Home")}>Home</li>
-//         <li onClick={() => props.setActivePage("About")}>About</li>
-//         <li onClick={() => props.setActivePage("Menu")}>Menu</li>
-//         <li onClick={() => props.setActivePage("Reservations")}>
-//           Reservations
-//         </li>
-//         <li onClick={() => props.setActivePage("Order")}>Order Online</li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
-function About(props) {
-  return (
-    <section id={props.id}>
-      <h2>About Us</h2>
-      <p>
-        We are a family-owned restaurant that has been serving our community for
-        over 20 years.
-      </p>
-    </section>
-  );
-}
-
-function Menu(props) {
-  return (
-    <section id={props.id}>
-      <h2>Our Menu</h2>
-      <p>Check out our delicious menu items:</p>
-      <ul>
-        <li>Appetizers</li>
-        <li>Entrees</li>
-        <li>Desserts</li>
-      </ul>
-    </section>
-  );
-}
-
-// function Reservations(props) {
-//   return (
-//     <section id={props.id}>
-//       <h2>Reservations</h2>
-//       <p>Make a reservation to dine with us:</p>
-//       <form>
-//         <input type="text" placeholder="Name" />
-//         <input type="email" placeholder="Email" />
-//         <input type="date" placeholder="Date" />
-//         <input type="time" placeholder="Time" />
-//         <button type="submit">Make Reservation</button>
-//       </form>
-//     </section>
-//   );
-// }
-
-// function Order(props) {
-//   return (
-//     <section id={props.id}>
-//       <h2>Order Online</h2>
-//       <p>Order online for pickup or delivery:</p>
-//       <form>
-//         <input type="text" placeholder="Name" />
-//         <input type="email" placeholder="Email" />
-//         <input type="text" placeholder="Address" />
-//         <button type="submit">Place Order</button>
-//       </form>
-//     </section>
-//   );
-// }
 export default App;
--- C8 Front-End Developer Capstone/capstone/src/components/Footer.js
+++ C8 Front-End Developer Capstone/capstone/src/components/Footer.js
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Link,
+  Stack,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
+import { IconButton, useColorModeValue } from "@chakra-ui/react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const navItems = [
+    { label: "Home", href: "#" },
+    { label: "About", href: "#" },
+    { label: "Menu", href: "#" },
+    { label: "Reservations", href: "#" },
+    { label: "Order Online", href: "#" },
+    { label: "Login", href: "#" },
+  ];
 
-export default Footer
+  const contactItems = [
+    { label: "1234 Main St, City, ST 12345", href: "#" },
+    { label: "(123) 456-7890", href: "#" },
+    { label: "info@restaurant.com", href: "#" },
+  ];
+
+  const socialLinks = [
+    {
+      icon: <FaFacebook />,
+      label: "Facebook",
+      href: "#",
+    },
+    {
+      icon: <FaInstagram />,
+      label: "Instagram",
+      href: "#",
+    },
+  ];
+
+  return (
+    <>
+      {/* <Box as="footer" bg="#333" color="white"></Box> */}
+
+      <Box bg={useColorModeValue("gray.100", "gray.900")} py="10">
+        <Stack direction="row" spacing={40} justify="center" align="start">
+          <Stack spacing={4} align="start">
+            <Text fontWeight="bold" fontSize="xl">
+              Doormat Navigation
+            </Text>
+            {navItems.map((item) => (
+              <Link key={item.label} href={item.href} fontSize="md">
+                {item.label}
+              </Link>
+            ))}
+          </Stack>
+          <Stack spacing={4} align="start">
+            <Text fontWeight="bold" fontSize="xl">
+              Contact
+            </Text>
+            {contactItems.map((item) => (
+              <Link key={item.label} href={item.href} fontSize="md">
+                {item.label}
+              </Link>
+            ))}
+          </Stack>
+          <Stack spacing={4} align="start">
+            <Text fontWeight="bold" fontSize="xl">
+              Social Media Links
+            </Text>
+            <HStack spacing={2}>
+              {socialLinks.map((item) => (
+                <IconButton
+                  key={item.label}
+                  aria-label={item.label}
+                  icon={item.icon}
+                  href={item.href}
+                  fontSize="xl"
+                  bg="transparent"
+                  _hover={{ bg: "transparent" }}
+                />
+              ))}
+            </HStack>
+          </Stack>
+        </Stack>
+        <Divider
+          mt="8"
+          borderColor={useColorModeValue("gray.400", "gray.600")}
+        />
+        <Flex justify="center" align="center" mt="8">
+          <Text fontSize="sm">
+            © 2023 Restaurant. All rights reserved. | Privacy Policy | Terms of
+            Service
+          </Text>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
